abf76cea15ee808bb627f5e9e6ad972b0c530fa9
Thomas Higginbotham <thomas.higginbotham@experis.com>
2017-10-25 12:08:12 -0400

    Style ButtonListComponent

--- pattern-lib/src/app/button-list/button-list.component.ts
+++ pattern-lib/src/app/button-list/button-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+import { UtilsService } from '../utils.service';
 
 @Component({
   selector: 'app-button-list',
@@ -10,10 +12,20 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ButtonListComponent implements OnInit {
+  @ViewChild('wrapper')wrapper: ElementRef;
 
-  constructor() { }
+  constructor(
+    private _utilsService: UtilsService,
+    private _renderer: Renderer2
+  ) { }
 
   ngOnInit() {
+    // Apply user's body styles to component
+    this._utilsService.applyStyleSheetStylesToElement(
+      '/assets/ext/css/main.css',
+      'body',
+      this.wrapper.nativeElement,
+      this._renderer
+    ).then();
   }
-
 }

8963377ccdbd94e0bbcb5596d8725f70f1a31459
Thomas Higginbotham <thomas.higginbotham@experis.com>
2018-09-26 08:45:23 -0400

    Add destroyComponents sample function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 /* jshint browser:true */
 
-var myScripts = {
-  initComponents: function() {
-    'use strict';
+var myScripts = (function() {
+  'use strict';
 
-    // Replace the following with your own code
-    console.log('Web components have loaded.');
-  }
-};
+  return {
+    initComponents: function() {
+      // Replace the following with your own code
+      console.log('Web components have loaded.');
+    },
+    destroyComponents: function() {
+      // The following code will run when components are destroyed in RPL
+      console.log('Web components have been unloaded');
+    }
+  };
+})();
 
 if (typeof window.RavenPatternLibrary === 'undefined') {
   // We're not in the Raven Pattern Library, so run scripts immediately
